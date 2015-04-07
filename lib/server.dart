35a5ad582e2ac242f0bdf4062838fd6047010f0f
Justin Andresen <jan.dresen.95@gmail.com>
2015-04-07 18:17:40 +0200

    Mark old `toml.browser` and `toml.server` library as deprecated.

--- lib/server.dart
+++ lib/server.dart
@@ -2,6 +2,8 @@
 // This software may be modified and distributed under the terms
 // of the MIT license. See the LICENSE file for details.
 
+/// Deprecated library. You should use the new `toml.loader` library.
+@deprecated
 library toml.server;
 
 import 'dart:async';
