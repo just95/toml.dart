e46d097e210b8e1657fc5356e6786c209e1f2fc8
Justin Andresen <jan.dresen.95@gmail.com>
2015-11-10 18:48:53 +0100

    Restrict `config_test` to `vm` platform.

--- test/config_test.dart
+++ test/config_test.dart
@@ -2,6 +2,7 @@
 // This software may be modified and distributed under the terms
 // of the MIT license. See the LICENSE file for details.
 
+@TestOn("vm")
 library toml.test.config_test;
 
 import 'package:toml/loader/fs.dart';
