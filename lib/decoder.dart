88729bbd9635d283747e921ef3b1d5fd42fb5fd4
Justin Andresen <jan.dresen.95@gmail.com>
2016-05-21 18:37:27 +0200

    Fix decoder library name.

--- lib/decoder.dart
+++ lib/decoder.dart
@@ -2,7 +2,7 @@
 // This software may be modified and distributed under the terms
 // of the MIT license. See the LICENSE file for details.
 
-library toml.encoder;
+library toml.decoder;
 
 import 'dart:collection';
 
