d7bd5a22f03e802e873d447dc37dbf15e52c8ff7
Justin Andresen <jan.dresen.95@gmail.com>
2016-05-21 18:37:57 +0200

    Add missing imports.

--- bin/encoder.dart
+++ bin/encoder.dart
@@ -4,6 +4,7 @@
 // This software may be modified and distributed under the terms
 // of the MIT license. See the LICENSE file for details.
 
+import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 
