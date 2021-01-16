23903b485587abd2c47f7172e1bd12ed0656be93
Justin Andresen <jan.dresen.95@gmail.com>
2021-01-16 21:47:39 +0100

    Fix date-time encoding tests for JavaScript
    In JavaScript DateTimes have millisecond precision only.

--- test/encoder/pretty_printer_test.dart
+++ test/encoder/pretty_printer_test.dart
@@ -54,25 +54,6 @@ void main() {
           ));
           expect(prettyPrinter.toString(), equals('1969-07-20T20:17:00.123Z'));
         });
-        test(
-          'pretty prints UTC date-time with milli- and microseconds correctly',
-          () {
-            var prettyPrinter = TomlPrettyPrinter();
-            prettyPrinter.visitDateTime(TomlDateTime(
-              DateTime.utc(1969, 7, 20, 20, 17, 0, 123, 456),
-            ));
-            expect(prettyPrinter.toString(),
-                equals('1969-07-20T20:17:00.123456Z'));
-          },
-        );
-        test('pretty prints UTC date-time with microseconds correctly', () {
-          var prettyPrinter = TomlPrettyPrinter();
-          prettyPrinter.visitDateTime(TomlDateTime(
-            DateTime.utc(1969, 7, 20, 20, 17, 0, 0, 456),
-          ));
-          expect(
-              prettyPrinter.toString(), equals('1969-07-20T20:17:00.000456Z'));
-        });
         test('pretty prints local date-time with offset', () {
           var prettyPrinter = TomlPrettyPrinter();
           prettyPrinter.visitDateTime(TomlDateTime(
