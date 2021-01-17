0a6cad06f1329a8adc979b9bbd3ef6cb8d5446ec
Justin Andresen <jan.dresen.95@gmail.com>
2021-01-17 21:41:11 +0100

    Implement parser for special floating point values #14

--- lib/src/ast/value/float.dart
+++ lib/src/ast/value/float.dart
@@ -10,27 +10,24 @@ import '../visitor/value.dart';
 ///
 ///     float = float-int-part ( exp / frac [ exp ] )
 ///     float =/ special-float
-///
-///     float-int-part = dec-int
-///     frac = decimal-point zero-prefixable-int
-///     decimal-point = %x2E               ; .
-///     zero-prefixable-int = DIGIT *( DIGIT / underscore DIGIT )
-///
-///     exp = "e" float-exp-part
-///     float-exp-part = [ minus / plus ] zero-prefixable-int
-///
-///     special-float = [ minus / plus ] ( inf / nan )
-///     inf = %x69.6e.66  ; inf
-///     nan = %x6e.61.6e  ; nan
-///
-/// TODO The special values `inf` and `nan` were added in TOML 0.5.0 and are
-/// not supported yet.
-///
-/// TODO Leading zeros in exponent were allowed in TOML 1.0.0-rc.1 and are not
-/// supported yet.
 class TomlFloat extends TomlValue<double> {
   /// Parser for a TOML floating point value.
-  static final Parser<TomlFloat> parser = (() {
+  static final Parser<TomlFloat> parser =
+      (finalFloatParser | specialFloatParser).cast<TomlFloat>();
+
+  /// Parser for a regular TOML floating point value.
+  ///
+  ///     float-int-part = dec-int
+  ///     frac = decimal-point zero-prefixable-int
+  ///     decimal-point = %x2E               ; .
+  ///     zero-prefixable-int = DIGIT *( DIGIT / underscore DIGIT )
+  ///
+  ///     exp = "e" float-exp-part
+  ///     float-exp-part = [ minus / plus ] zero-prefixable-int
+  ///
+  /// TODO Leading zeros in exponent were allowed in TOML 1.0.0-rc.1 and are not
+  /// supported yet.
+  static final Parser<TomlFloat> finalFloatParser = (() {
     var digits = digit().plus().separatedBy(char('_'));
     var decimal = anyOf('+-').optional() & (char('0') | digits);
     var exp = anyOf('eE') & anyOf('+-').optional() & digits;
@@ -39,6 +36,22 @@ class TomlFloat extends TomlValue<double> {
     return float
         .flatten()
         .map((str) => TomlFloat(double.parse(str.replaceAll('_', ''))));
+  })();
+
+  /// Parser for a special TOML floating point value.
+  ///
+  ///     special-float = [ minus / plus ] ( inf / nan )
+  ///     inf = %x69.6e.66  ; inf
+  ///     nan = %x6e.61.6e  ; nan
+  static final Parser<TomlFloat> specialFloatParser = (() {
+    var plus = char('+').map((_) => 1.0);
+    var minus = char('-').map((_) => -1.0);
+    var sign = plus | minus;
+    var inf = string('inf').map((_) => double.infinity);
+    var nan = string('nan').map((_) => double.nan);
+    return (sign & (inf | nan))
+        .castList<double>()
+        .map((pair) => TomlFloat(pair.reduce((sign, value) => sign * value)));
   })();
 
   @override
