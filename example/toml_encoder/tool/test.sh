76b4553aaf3c1701245f8f1e36119b5efee90ebf
Justin Andresen <jan.dresen.95@gmail.com>
2021-01-17 01:30:02 +0100

    Add test scripts to examples
    The test scripts are executed by the `./tool/test-examples.sh`
    script.

---
+++ example/toml_encoder/tool/test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# This script is used by the CI pipeline to execute this example.
+
+# Run the `bin/example.dart` script and remember output.
+output=$(dart pub run example)
+echo "$output"
+
+# Compare output with expected output.
+expected_output="\
+[shape]
+type = 'rectangle'
+
+[[shape.points]]
+x = 1
+y = 1
+
+[[shape.points]]
+x = 1
+y = -1
+
+[[shape.points]]
+x = -1
+y = -1
+
+[[shape.points]]
+x = -1
+y = 1"
+if [[ "$output" != "$expected_output" ]]; then
+  exit 1
+fi
---
+++ example/toml_parser/tool/test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script is used by the CI pipeline to execute this example.
+
+# Run the `bin/example.dart` script and remember output.
+output=$(dart pub run example < config.toml)
+echo "$output"
+
+# Compare output with expected output.
+if [[ "$output" != "Hello, World!" ]]; then
+  exit 1
+fi
