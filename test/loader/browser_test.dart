1a063556242aaa03d02b22531d188b62084e52c3
Justin Andresen <jan.dresen.95@gmail.com>
2021-01-24 00:37:40 +0100

    Add test for files with UTF-8 BOM #17

--- test/loader/browser_test.dart
+++ test/loader/browser_test.dart
@@ -14,6 +14,17 @@ void main() {
           completion(equals('Test\n')),
         );
       });
+      test('load test file with UTF-8 BOM', () {
+        // When a file with BOM is loaded in the browser, the Byte Order Mark
+        // is removed by the loader. Thus, the TOML parser does not have to
+        // take the BOM into account.
+        expect(
+          loadFile('../asset/bom.txt'),
+          completion(equals(
+            'This file starts with a UTF-8 Byte Order Mark (BOM).\n',
+          )),
+        );
+      });
     });
   });
 }
