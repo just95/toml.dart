12b1505b084baf60feb3827ef9513df57a0c6065
Justin Andresen <jan.dresen.95@gmail.com>
2022-01-18 14:45:09 +0100

    Write missing comments

--- lib/src/accessor/key/printer.dart
+++ lib/src/accessor/key/printer.dart
@@ -13,6 +13,8 @@ class TomlAccessorKeyPrinter with TomlAccessorKeyVisitorMixin<bool> {
   /// Buffer for constructing the formatted string.
   final StringBuffer _buffer = StringBuffer();
 
+  /// An AST builder that is used to convert the names of accessor keys to
+  /// key nodes that can be printed with the pretty printer.
   final TomlAstBuilder _astBuilder = TomlAstBuilder();
 
   @override
@@ -29,6 +31,7 @@ class TomlAccessorKeyPrinter with TomlAccessorKeyVisitorMixin<bool> {
 
   @override
   bool visitNameKey(TomlNameAccessorKey key) {
+    // Separate the key from the parent key if the parent is not the root key.
     if (key.parentKey.acceptKeyVisitor(this)) _buffer.write('.');
     _buffer.write(_astBuilder.buildSimpleKey(key.name));
     return true;
