4c0cdb45b80c406b4539f82f5d1c59cfe578f03a
Justin Andresen <jan.dresen.95@gmail.com>
2022-05-31 15:36:24 +0200

    Fix comment

--- tool/format-examples.sh
+++ tool/format-examples.sh
@@ -18,7 +18,7 @@ script_dir=$(dirname "$script")
 root_dir=$(dirname "$script_dir")
 cd "$root_dir"
 
-# Find all examples with a `pubspec.yaml` file.
+# Format code of all `*.dart` files in the `example` directory.
 echo "Checking formatting of all examples..."
 if ! dart format "$@" example 2>&1 | awk '{print " | " $0}'; then
   echo "------------------------------------------------------------------"
