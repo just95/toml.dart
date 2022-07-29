a681716de21e1fb337a258c7c23fc731d77f8163
Justin Andresen <jan.dresen.95@gmail.com>
2022-07-29 11:54:51 +0200

    Move accessor `is*` and `expect*` methods to extension

--- lib/src/accessor.dart
+++ lib/src/accessor.dart
@@ -3,6 +3,7 @@ library toml.src.accessor;
 export 'accessor/key.dart';
 export 'accessor/key/printer.dart';
 export 'accessor/tree.dart';
+export 'accessor/tree/cast.dart';
 export 'accessor/tree/match.dart';
 export 'accessor/tree/to_value.dart';
 export 'accessor/visitor/key.dart';
--- lib/src/accessor/tree.dart
+++ lib/src/accessor/tree.dart
@@ -26,42 +26,6 @@ abstract class TomlAccessor {
   /// The type of the value represented by this node.
   TomlValueType get type;
 
-  /// Whether this node represents an array of tables or array value.
-  bool get isArray => this is TomlArrayAccessor;
-
-  /// Whether this node represents a table or inline table.
-  bool get isTable => this is TomlTableAccessor;
-
-  /// Whether this node represents a non-composite value.
-  bool get isValue => this is TomlValueAccessor;
-
-  /// Ensures that this node is of the given type.
-  ///
-  /// Returns this node or throws a [TomlValueTypeException] if the runtime
-  /// type don't match [T]. The [expectedTypes] are not used for the comparison
-  /// but only for the error message in case of a mismatch.
-  T _expectType<T extends TomlAccessor>(Set<TomlValueType> expectedTypes) {
-    var self = this;
-    if (self is T) return self;
-    throw TomlValueTypeException(
-      nodeName,
-      expectedTypes: expectedTypes,
-      actualType: type,
-    );
-  }
-
-  /// Ensures that this node is a [TomlArrayAccessor].
-  TomlArrayAccessor expectArray() =>
-      _expectType<TomlArrayAccessor>({TomlValueType.array});
-
-  /// Ensures that this node is a [TomlTableAccessor].
-  TomlTableAccessor expectTable() =>
-      _expectType<TomlTableAccessor>({TomlValueType.table});
-
-  /// Ensures that this node is a [TomlValueAccessor].
-  TomlValueAccessor expectValue() =>
-      _expectType<TomlValueAccessor>(TomlPrimitiveValue.types);
-
   /// Invokes the correct `visit*` method for this value of the given visitor.
   R acceptVisitor<R>(TomlAccessorVisitor<R> visitor);
 }
