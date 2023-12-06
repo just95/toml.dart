605c4dc42b8e1aa0df2ad5d0abea289df28f6c15
Erlend Lind Madsen <erlendf80@gmail.com>
2023-12-06 07:06:50 +0100

    ml_basic: remove <dynamic> from selectFarthestJoined

--- lib/src/ast/value/string/ml_basic.dart
+++ lib/src/ast/value/string/ml_basic.dart
@@ -90,7 +90,7 @@ class TomlMultilineBasicString extends TomlMultilineString {
               tomlNewline &
               ChoiceParser(
                 [tomlWhitespaceChar, tomlNewline],
-                failureJoiner: selectFarthestJoined<dynamic>,
+                failureJoiner: selectFarthestJoined,
               ).star())
           .map((_) => '');
 
@@ -100,7 +100,7 @@ class TomlMultilineBasicString extends TomlMultilineString {
     var buffer = StringBuffer();
     var unescapedOrNewline = ChoiceParser(
       [unescapedParser, tomlNewline],
-      failureJoiner: selectFarthestJoined<dynamic>,
+      failureJoiner: selectFarthestJoined,
     );
     var quotes = 0;
     var iterator = value.runes.iterator;
