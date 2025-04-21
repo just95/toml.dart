560cb473149ea54755f9d9c91fdaf7016ea8f1da
Justin Andresen <jan.dresen.95@gmail.com>
2025-04-21 14:07:59 +0200

    Use `package:web` instead of `dart:html` for loading TOML documents on the web

---
+++ lib/src/loader/exception/unexpected_http_status.dart
@@ -0,0 +1,33 @@
+library toml.src.loader.exception.unexpected_http_status;
+
+import 'package:meta/meta.dart';
+
+import '../../exception.dart';
+
+/// An exception which is thrown when the web loader encounters an HP status
+/// code that is not in the range of 200-299.
+@immutable
+class TomlUnexpectedHttpStatusException extends TomlException {
+  /// The HTTP status code.
+  final int status;
+
+  /// A human-readable explanation of the [[status]] code.
+  ///
+  /// Example: "Not Found" for status code 404.
+  final String statusText;
+
+  /// Creates a new exception for an unexpected HTTP status code.
+  TomlUnexpectedHttpStatusException(this.status, this.statusText);
+
+  @override
+  String get message => 'Unexpected HTTP status code: $status $statusText';
+
+  @override
+  bool operator ==(Object other) =>
+      other is TomlUnexpectedHttpStatusException &&
+      other.status == status &&
+      other.statusText != statusText;
+
+  @override
+  int get hashCode => status ^ statusText.hashCode;
+}
---
+++ lib/src/loader/web.dart
@@ -0,0 +1,32 @@
+library toml.src.loader.web;
+
+import 'dart:async';
+import 'dart:convert';
+import 'dart:js_interop';
+import 'package:web/web.dart' as web;
+
+import 'exception/unexpected_http_status.dart';
+
+/// Loads a configuration file from the server via HTTP using `package:web`.
+Future<String> loadFile(String filename) async {
+  var response = await web.window.fetch(filename.toJS).toDart;
+  if (!response.ok) {
+    throw TomlUnexpectedHttpStatusException(
+      response.status,
+      response.statusText,
+    );
+  }
+
+  // Instead of using `response.text()` to get the string, we use
+  // `response.bytes()` to get the raw bytes and decode them ourselves
+  // to ensure that an exception is thrown if the file is not a valid UTF-8.
+  var buffer = await response.bytes().toDart;
+  return utf8.decode(buffer.toDart);
+}
+
+/// Throws an [UnsupportedError] because loading configuration files
+/// synchronously via HTTP is not supported using `package:web`.
+String loadFileSync(String filename) => throw UnsupportedError(
+      'Cannot load file "$filename" synchronously: Configuration files cannot '
+      'be loaded synchronously on the web.',
+    );
