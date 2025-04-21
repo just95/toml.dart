40ae976cd12dd1a384718af30fac194245dd0fe2
Justin Andresen <jan.dresen.95@gmail.com>
2025-04-21 14:35:26 +0200

    Migrate `http_config_loader` example to `package:web`

--- example/http_config_loader/web/example.dart
+++ example/http_config_loader/web/example.dart
@@ -1,16 +1,18 @@
 import 'dart:async';
-import 'dart:html';
 
 import 'package:toml/toml.dart';
+import 'package:web/web.dart' as web;
 
 Future main() async {
-  var elem = document.getElementById('text')!;
-  try {
-    var document = await TomlDocument.load('config.toml');
-    var config = document.toMap();
-    elem.text = config['table']['array'][0]['key'].toString();
-  } catch (e) {
-    elem.style.color = 'red';
-    elem.text = 'ERROR: $e';
+  var elem = web.document.getElementById('text')!;
+  if (elem is web.HTMLElement) {
+    try {
+      var document = await TomlDocument.load('config.toml');
+      var config = document.toMap();
+      elem.textContent = config['table']['array'][0]['key'].toString();
+    } catch (e) {
+      elem.style.color = 'red';
+      elem.textContent = 'ERROR: $e';
+    }
   }
 }
